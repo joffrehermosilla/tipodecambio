56dd797646e0dc3921e448a3c23239041e8d6ed6
joffre <joffre.hermosilla@gmail.com>
2024-04-28 21:57:15 -0500

    chore[15] configuracion crsf lambda

--- src/main/java/backend/joffre/infrastructure/config/SecurityConfig.java
+++ src/main/java/backend/joffre/infrastructure/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,6 +21,7 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((authHttp) -> authHttp
 				.requestMatchers(HttpMethod.GET,"/authorized").permitAll()
+				.requestMatchers(HttpMethod.GET,"/h2-ui/**").permitAll().anyRequest().authenticated()
 				.requestMatchers(HttpMethod.GET, "/list").hasAnyAuthority("SCOPE_read","SCOPE_write")
 				.requestMatchers(HttpMethod.POST, "/create").hasAuthority("SCOPE_write")
 				.anyRequest().authenticated())
@@ -28,6 +30,10 @@ public class SecurityConfig {
 				.oauth2Login(login -> login.loginPage("/oauth2/authorization/client-app"))
 				.oauth2Client(withDefaults())
 				.oauth2ResourceServer(resourceServer -> resourceServer.jwt(withDefaults()));
+		http.csrf(csrf -> csrf
+	            .ignoringRequestMatchers( "/h2-ui")
+	            .disable());
+		http.headers(headers -> headers.frameOptions(FrameOptionsConfig::disable));
 				return http.build();				
 				
 	}
